6a97de3e2f0064dbe483a38fcc06fdaf42660624
Mihai Vasilian <grayasm@gmail.com>
2022-06-19 17:59:40 +0200

    change at 17:58

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main()
     printf("%s\n", "v.2022-06-19_1720");
     printf("%s\n", "changed at 2022-06-19_1727");
     printf("%s\n", "changed at 2022-06-19_1728");
+    printf("%s\n", "changed at 2022-06-19_1758");
     return 0;
 }
