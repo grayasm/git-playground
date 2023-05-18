866ce5e4afaefbb71b712647dac62d5bcc96b092
mihai <mihai@binary.fritz.box>
2023-05-18 09:30:31 +0200

    main at 2023-05-18 09:29

--- main.cpp
+++ main.cpp
@@ -2,9 +2,6 @@
 
 int main()
 {
-  printf("%s\n", "main at 2023-04-04 18:25:00");
-  printf("%s\n", "line at 2023-04-04 18:34:00");
-  printf("%s\n", "line at 2023-04-04 18:35:00");
-  printf("%s\n", "main at 2023-04-04 18:37:00");
+  printf("%s\n", "main at 2023-05-18 09:29:00");
   return 0;
 }
